a80be657e1062a16c1a5fdecf8add8f2724ee8a9
meow87wang <1999119wang@gmail.com>
2019-04-07 03:30:52 +0800

    quicksort!

---
+++ quicksort.c
@@ -0,0 +1,57 @@
+// borrow from "https://www.geeksforgeeks.org/quick-sort/""
+
+/* C implementation QuickSort */
+#include<stdio.h> 
+
+#include "process_control.h"
+
+// A utility function to swap two elements 
+void swap(Process* a, Process* b) 
+{ 
+    Process t = *a; 
+    *a = *b; 
+    *b = t; 
+} 
+  
+/* This function takes last element as pivot, places 
+   the pivot element at its correct position in sorted 
+    array, and places all smaller (smaller than pivot) 
+   to left of pivot and all greater elements to right 
+   of pivot */
+int partition (Process arr[], int low, int high) 
+{ 
+    Process pivot = arr[high];    // pivot 
+    int i = (low - 1);  // Index of smaller element 
+  
+    for (int j = low; j <= high- 1; j++) 
+    { 
+        // If current element is smaller than or 
+        // equal to pivot 
+        if (arr[j].ready_time <= pivot.ready_time) 
+        { 
+            i++;    // increment index of smaller element 
+            swap(&arr[i], &arr[j]); 
+        } 
+    } 
+    swap(&arr[i + 1], &arr[high]); 
+    return (i + 1); 
+} 
+  
+/* The main function that implements QuickSort 
+ arr[] --> Array to be sorted, 
+  low  --> Starting index, 
+  high  --> Ending index */
+void quickSort(Process arr[], int low, int high) 
+{ 
+    if (low < high) 
+    { 
+        /* pi is partitioning index, arr[p] is now 
+           at right place */
+        int pi = partition(arr, low, high); 
+  
+        // Separately sort elements before 
+        // partition and after partition 
+        quickSort(arr, low, pi - 1); 
+        quickSort(arr, pi + 1, high); 
+    } 
+} 
