df7888931d02d307da71058b3b0da147092f57dc
fashing1999 <31907914+fashing1999@users.noreply.github.com>
2019-04-09 23:36:17 +0800

    Update scheduler_FIFO.c

--- scheduler_FIFO.c
+++ scheduler_FIFO.c
@@ -8,6 +8,7 @@
 int scheduler_FIFO(Process *proc, int N_procs){
 
 	int cur = -1;
+	int total_time=0;
 
 	while(1){
 
@@ -15,12 +16,17 @@ int scheduler_FIFO(Process *proc, int N_procs){
 		if( cur >= N_procs ) break;
 
 		pid_t chpid = proc_create(proc[cur]);
-
+		while( proc[cur].ready_time > total_time ){
+			TIME_UNIT();
+			// wait 1 time unit until proc is ready
+			total_time += 1;
+		}
 		while( proc[cur].exec_time > 0 ){
 			// tell process to run 1 time unit
 			write(proc[cur].pipe_fd[1], "run", strlen("run"));
 			// run 1 time unit itself
 			TIME_UNIT();
+			total_time += 1;
 			proc[cur].exec_time -= 1;
 		}
 
